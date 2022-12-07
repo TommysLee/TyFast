7c6366579ff9b8a2f54ddc17f6086e0b29737f3e
Tommy <li_zongjin@qq.com>
2022-12-07 21:47:45 +0800

    新增：Excel读取工具类；KeyLock细粒度资源锁

---
+++ tyfast-common/src/main/java/com/ty/cm/utils/document/JxlsUtils.java
@@ -0,0 +1,55 @@
+package com.ty.cm.utils.document;
+
+import com.google.common.collect.Lists;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.List;
+
+import static com.ty.cm.constant.Ty.HLINE;
+
+/**
+ * JXLS扩展的自定义函数
+ *
+ * @Author Tommy
+ * @Date 2022/12/7
+ */
+public class JxlsUtils {
+
+    /**
+     * 初始化整型List并赋值
+     *
+     * @param size 集合大小
+     * @return List<Integer>
+     */
+    public List<Integer> intList(int size) {
+        List<Integer> list = Lists.newArrayListWithCapacity(size);
+        for (int i = 0; i < size;) {
+            list.add(++i);
+        }
+        return list;
+    }
+
+    /**
+     * 如果字符串为空或null则返回默认值
+     *
+     * @param text
+     * @param defaultVal
+     * @return String
+     */
+    public String defaultIfBlank(String text, Integer defaultVal) {
+        return StringUtils.defaultIfBlank(text, null != defaultVal ? defaultVal.toString() : StringUtils.EMPTY);
+    }
+
+    /**
+     * 如果对象为空则以横线替换
+     *
+     * @param text
+     * @param defaultVal
+     * @return String
+     */
+    public Object defaultHIfBlank(Object obj) {
+        return null == obj? HLINE : obj;
+    }
+}
+
+
